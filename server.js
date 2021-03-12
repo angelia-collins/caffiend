66f864bb0fae98348ee4a4fa63c85d55f9c733eb
angelia collins <angiecollins09@gmail.com>
2021-03-11 20:06:06 -0600

    adding packages

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const passport = require("./config/passport");
+const db = require("./models");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+}
+app.use(
+    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  );
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(express.static(__dirname + 'public/'));
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
