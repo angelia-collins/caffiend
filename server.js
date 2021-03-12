1e7f8afba7eb8f079683fa5d04a5d750b3e14cb6
angelia collins <angiecollins09@gmail.com>
2021-03-11 20:36:11 -0600

    trying to add passport

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"));
+// }
 app.use(
     session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
   );
@@ -22,11 +22,14 @@ app.use(passport.session());
 
 app.use(express.static(__dirname + 'public/'));
 
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
