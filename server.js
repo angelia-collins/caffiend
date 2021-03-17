2c691a939947cebec4defca5377359a34d60f8aa
angelia collins <angiecollins09@gmail.com>
2021-03-16 19:41:05 -0500

    redo

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const session = require("express-session");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
-const passport = require("./config/passport");
-const db = require("./models");
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
 
-// Define middleware here
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("build"));
-// }
-app.use(
-    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-  );
-app.use(passport.initialize());
-app.use(passport.session());
 
+// Static directory
+app.use(express.static("public"));
 
-app.use(express.static(__dirname + 'public/'));
-
-require("./routes/html-routes.js")(app);
+// Routes
+// =============================================================
 require("./routes/api-routes.js")(app);
+// require("./routes/author-api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./build/index.html"));
-// });
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
