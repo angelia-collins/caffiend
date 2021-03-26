2ecc19fb8fc8ffbd9493dcd652f045de6035d3b8
angelia collins <angiecollins09@gmail.com>
2021-03-25 20:35:11 -0500

    adding d3 bar chart component

---
+++ client/src/components/BarChart.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import * as d3 from "d3";
+import "./BarChart.css";
+
+class BarChart extends Component {
+    componentDidMount() {
+      this.drawChart();
+    }
+      
+    drawChart() {
+      const data = [12, 5, 6, 6, 9, 10];
+      
+      const svg = d3.select("body")
+      .append("svg")
+      .attr("width", 300)
+      .attr("height", 500)
+      .style("margin-bottom", 100)
+      .style("margin-left", 100);
+   
+                    
+      svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => i * 70)
+        .attr("y", (d, i) => 500 - 10 * d)
+        .attr("width", 65)
+        .attr("height", (d, i) => d * 10)
+        .attr("fill", "green")
+    }
+          
+    render(){
+      return <div id={"#" + this.props.id}></div>
+    }
+  }
+      
+  export default BarChart;
