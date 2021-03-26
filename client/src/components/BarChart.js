fd2105f5814a582f4dc74c6207a58a4ee9706235
angelia collins <angiecollins09@gmail.com>
2021-03-25 21:25:16 -0500

    working on the barchart

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -8,14 +8,18 @@ class BarChart extends Component {
     }
       
     drawChart() {
-      const data = [12, 5, 6, 6, 9, 10];
+        const data = [12, 5, 6, 6, 9, 10];
+
+    //   const data = this.props.data;
       
-      const svg = d3.select("body")
-      .append("svg")
-      .attr("width", 300)
+      const svg = d3.select("body").append("svg")
+    //   .attr("width", this.props.width)
+    //   .attr("height", this.props.height)
+      .attr("width", 500)
       .attr("height", 500)
-      .style("margin-bottom", 100)
-      .style("margin-left", 100);
+    //   .style("margin-bottom", 100)
+      .style("margin-left", 100)
+    //   .style('.bar-stuff')
    
                     
       svg.selectAll("rect")
@@ -26,7 +30,17 @@ class BarChart extends Component {
         .attr("y", (d, i) => 500 - 10 * d)
         .attr("width", 65)
         .attr("height", (d, i) => d * 10)
-        .attr("fill", "green")
+        .attr("fill", "blue")
+        // .attr('margin-left', '100px')
+
+        svg.selectAll("text")
+  .data(data)
+  .enter()
+  .append("text")
+  .text((d) => d)
+  .attr("x", (d, i) => i * 70)
+//   .attr("y", (d, i) => h - (10 * d) - 3)
+  .attr("y", (d, i) => 200 - (10 * d) - 3);
     }
           
     render(){
--- client/src/pages/Budget.js
+++ client/src/pages/Budget.js
@@ -7,11 +7,21 @@ import BarChart from '../components/BarChart';
 
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 
+// class App extends React.Component {
+  
+//   state = {
+//     data: [12, 5, 6, 6, 9, 10],
+//     width: 700,
+//     height: 500,
+//     // id: root
+//   }}
+
 export const BudgetComponent = () => {
   return (
     <Container className="mb-5">
       <Header>Monthly Budget</Header>
       <BarChart />
+      {/* <BarChart data={this.state.data} width={this.state.width} height={this.state.height} /> */}
     </Container>
   );
 };
